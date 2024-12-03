96e31fd795287fad646a3e1a6724d148a430d477
theotrin <theopinheiro12@gmail.com>
2024-12-03 18:57:48 -0400

    feat: adcionado caso para quando o banco de dados estiver povoado, gerar uma tela

--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/components/task.dart';
+import 'package:project/data/task_dao.dart';
 import 'package:project/data/task_inherit.dart';
 import 'package:project/screens/form_screen.dart';
 
@@ -15,12 +16,39 @@ class _InitialScreenState extends State<InitialScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter: Primeiros Passos'),
+        title: const Text('Tarefas: '),
         leading: Icon(Icons.add_task),
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 8, bottom: 70),
-        child: Container(),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  // TODO: Handle this case.
+                  break;
+                case ConnectionState.waiting:
+                  // TODO: Handle this case.
+                  break;
+                case ConnectionState.active:
+                  // TODO: Handle this case.
+                  break;
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task tarefa = items[index];
+                            return tarefa;
+                          });
+                    }
+                  }
+                  break;
+              }
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
