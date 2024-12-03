90300a45d43dd6ee8df0fe4af6ee280746d78403
Theo Pinheiro <19900@bemol.local>
2024-12-03 08:40:19 -0400

    feat: começando a implementar método

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:project/components/task.dart';
+import 'package:project/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -12,7 +14,17 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task task) async {}
-  Future<List<Task>> findAll () async {}   
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll: ');
+
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
+    print('Procurando dados no banco de dados... Econtrado: $result');
+
+    return toList();
+  }
+
   Future<List<Task>> find(String taskName) async {}
-  delete (String taskName) async {}
+  delete(String taskName) async {}
 }
